453e849ecf1031b81431d5ee69fd36bb542a5b16
Stuart Jones Jr <stuartjonesjr@Stuarts-Mac-mini-2.local>
2026-01-27 12:32:32 -0800

    Fix Vite paths and Netlify routing

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/se_project_final/",
+  base: "./",
   plugins: [react()],
   // server: {
   //   port: 3000, // Change the port number to 3000
