0589bf7befd5026966a8ba5287d634440da14717
Stuart Jones Jr <stuartjonesjr@Stuarts-Mac-mini-2.local>
2026-01-27 14:13:49 -0800

    remove fetch import

--- netlify/functions/news.js
+++ netlify/functions/news.js
@@ -1,16 +1,21 @@
-import fetch from "node-fetch";
+export async function handler(event) {
+  try {
+    const { q, from, to } = event.queryStringParameters;
 
-export async function handler(event, context) {
-  const { q, from, to } = event.queryStringParameters;
+    const response = await fetch(
+      `https://newsapi.org/v2/everything?q=${q}&from=${from}&to=${to}&pageSize=100&language=en&apiKey=${process.env.NEWS_API_KEY}`,
+    );
 
-  const res = await fetch(
-    `https://newsapi.org/v2/everything?q=${q}&from=${from}&to=${to}&pageSize=100&language=en&apiKey=${process.env.NEWS_API_KEY}`,
-  );
+    const data = await response.json();
 
-  const data = await res.json();
-
-  return {
-    statusCode: 200,
-    body: JSON.stringify(data),
-  };
+    return {
+      statusCode: 200,
+      body: JSON.stringify(data),
+    };
+  } catch (error) {
+    return {
+      statusCode: 500,
+      body: JSON.stringify({ error: error.message }),
+    };
+  }
 }
