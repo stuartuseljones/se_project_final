afa5f7593eff750aa3e2bc45e0685fab1e490aea
Stuart Jones Jr <stuartjonesjr@Stuarts-Mac-mini-2.local>
2026-01-27 10:28:40 -0800

    vite config

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,11 @@
 import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
 import React from "react";
+import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   base: "/se_project_final/",
-  // plugins: [react()],
+  plugins: [react()],
   // server: {
   //   port: 3000, // Change the port number to 3000
   // },
